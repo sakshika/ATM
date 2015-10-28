292a95f49fcfdfae9812553d32dc0dc90d01abe8
sakshi <sakshik@cybage.com>
2015-10-28 22:52:21 +0530

    build folder deleted and commented composer updated in run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ cd /opt
 which composer 
 composer config http-basic.example.org sakshika S@ksh!1991
 #composer config -g github-oauth.github.com <oauthtoken>
-composer update
+#composer update
+composer install
 ant -version
 ant
 ls
