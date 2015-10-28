731bcf5aa991ad46bdc850b33cedee589c4374cd
sakshi <sakshik@cybage.com>
2015-10-28 18:30:46 +0530

    stash

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cd /opt
 which composer 
+composer config http-basic.example.org abhijeetka abhijeet123#
+#composer config -g github-oauth.github.com <oauthtoken>
 composer update
 ant -version
 ant
