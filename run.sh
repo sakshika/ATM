d2223fa359238de5a3c8f7f5b1ce3f17d619b5de
sakshi <sakshik@cybage.com>
2015-10-29 02:01:52 +0530

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /opt
 which composer 
-#composer config http-basic.example.org abhijeetka abhijeet123#
+composer config http-basic.example.org abhijeetka abhijeet123#
 #composer config -g github-oauth.github.com 0a3258c80976f2dfd65ba75f09e97d58937515a3
 #composer update
 composer install
